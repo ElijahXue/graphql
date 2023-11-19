3d917c6e5e3b25d0374d34f62b413cd09b9342f5
Yongjie Xue <yxue9@huskers.unl.edu>
2023-11-19 14:43:24 -0600

    connect graphql with DisplayLocation function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+type Company = {
+  ceo: string;
+};
+
+type Roadster = {
+  apoapsis_au: number;
+};
+
+type Landpad = {
+  wikipedia: string;
+  status: string;
+  id: string;
+  full_name: string;
+};
+
+// Define a type for the overall data structure
+type QueryData = {
+  company: Company;
+  roadster: Roadster;
+  landpads: Landpad[];
+};
+
+const GET_DATA = gql`
+query ExampleQuery {
+  company {
+    ceo
+  }
+  roadster {
+    apoapsis_au
+  }
+  landpads {
+    wikipedia
+    status
+    id
+    full_name
+  }
+}
+`
+
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery<QueryData>(GET_DATA);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div>
+      {data && data.landpads.map((landpad) => (
+        <div key={landpad.id}>
+          <p>{landpad.full_name} - Status: {landpad.status}</p>
+        
+          <a href={landpad.wikipedia}>Wikipedia</a>
+          <p>{data?.company.ceo}</p>
+        </div>
+      ))}
+      
+    </div>
+  );
+}
+
 
 function App() {
+  const { loading, error, data } = useQuery(GET_DATA);
+  useEffect(() => {
+    console.log({ loading, error, data })
+
+  })
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My first Apollo app 🚀</h2>
+
+
+      <br />
+      <DisplayLocations />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+let client = new ApolloClient({
+  uri: 'https://spacex-production.up.railway.app/.',
+  cache: new InMemoryCache(),
+});
+// const client = ...
+
+// const client = ...
+
+// client
+//   .query({
+//     query: gql`
+//     query ExampleQuery {
+//       company {
+//         ceo
+//       }
+//       roadster {
+//         apoapsis_au
+//       }
+//       landpads {
+//         wikipedia
+//         status
+//       }
+//     }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
+  root.render(
+    <React.StrictMode>
+      <ApolloProvider client = {client}>
+  
+     
+      <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+  
+  
